fcdf78654decd5656c440d2f18556985dd9f7d58
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2023-01-04 23:07:14 -0100

    finished instiate mesages for nft and staking

---
+++ target/debug/deps/schema-51a74558e8b94c51.d
@@ -0,0 +1,8 @@
+/Users/saranevespereira/Documents/WBA_bootcamp/capstone/cw-staking-angel-0.2.3/target/debug/deps/schema-51a74558e8b94c51.rmeta: contracts/staking/src/bin/schema.rs
+
+/Users/saranevespereira/Documents/WBA_bootcamp/capstone/cw-staking-angel-0.2.3/target/debug/deps/schema-51a74558e8b94c51.d: contracts/staking/src/bin/schema.rs
+
+contracts/staking/src/bin/schema.rs:
+
+# env-dep:CARGO_PKG_NAME=staking
+# env-dep:CARGO_PKG_VERSION=0.1.0
---
+++ target/debug/deps/schema-e6ca55b85da8d869.d
@@ -0,0 +1,8 @@
+/Users/saranevespereira/Documents/WBA_bootcamp/capstone/cw-staking-angel-0.2.3/target/debug/deps/schema-e6ca55b85da8d869.rmeta: contracts/agent/src/bin/schema.rs
+
+/Users/saranevespereira/Documents/WBA_bootcamp/capstone/cw-staking-angel-0.2.3/target/debug/deps/schema-e6ca55b85da8d869.d: contracts/agent/src/bin/schema.rs
+
+contracts/agent/src/bin/schema.rs:
+
+# env-dep:CARGO_PKG_NAME=agent
+# env-dep:CARGO_PKG_VERSION=0.1.0
